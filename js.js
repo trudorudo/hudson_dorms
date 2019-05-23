32eeaa419351a92e91e936c66917fad53a3b0042
Kristina <k.trudorudo@gmail.com>
2019-05-23 16:02:04 -0400

    updates

---
+++ js.js
@@ -0,0 +1,18 @@
+
+  $(document).ready(function(){
+    $('.parallax').parallax();
+    $('.carousel').carousel(
+        {
+          dist: 0,
+          padding: 0,
+          fullWidth: true,
+          indicators: true,
+          duration: 100,
+        }
+        );
+        autoplay()   
+    function autoplay() {
+        $('.carousel').carousel('next');
+        setTimeout(autoplay, 4500);
+    }
+  });
